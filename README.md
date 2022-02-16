d2c255e2f662dbb61983472ef31b88227b10c464
WuJunFeng <xiguashuang5@gmail.com>
2022-02-16 13:20:25 +0800

    button

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 一些组件
 
-## lerna
+## 工具
+
+## 1.lerna 多包管理
+
+## 2.father-build 打包工具
+
+## 3.dumi 文档加 demo
+
+## 4.umi-test 测试
--- packages/cmp-react/src/Button/index.tsx
+++ packages/cmp-react/src/Button/index.tsx
@@ -1,10 +1,15 @@
 import React, { MouseEventHandler } from 'react';
-const a = 'a';
+import c from './index.less';
+
 export interface IButtonProps {
   children: React.ReactChild;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const Button: React.FC<IButtonProps> = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button onClick={onClick} className={c.cmpBtn}>
+      {children}
+    </button>
+  );
 };
 export default Button;
